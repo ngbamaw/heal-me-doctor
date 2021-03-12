0e23261565d9874159bf2d500cd78a6aed3c3cb1
PellYm785 <williamliya@hotmail.fr>
2021-03-12 11:46:14 +0100

    feat: create messanger elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Style from './style';
+import SendIcon from './assets/send-icon.png';
+
+const ReceiverMessage: React.FC<{ children: string }> = ({ children }) => (
+    <div className="message receiver">
+        <p>{children}</p>
+    </div>
+);
+
+const MeMessage: React.FC<{ children: string }> = ({ children }) => (
+    <div className="message me">
+        <p>{children}</p>
+    </div>
+);
 
 const App: React.FC = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Style>
+            <div className="discussion-section">
+                <ReceiverMessage>Hello sir</ReceiverMessage>
+                <MeMessage>Hello, you</MeMessage>
+                <MeMessage>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut
+                    commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat Lorem
+                    ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut
+                    commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat Lorem
+                    ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut
+                    commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat Lorem
+                    ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut
+                    commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat
+                </MeMessage>
+            </div>
+            <div className="message-choices">
+                <div className="list-messages">
+                    <div className="choice selected">
+                        <p>Good bye</p>
+                    </div>
+                </div>
+                <button className="send-btn" type="button">
+                    <img src={SendIcon} alt="send" />
+                </button>
+            </div>
+        </Style>
     );
 };
 
