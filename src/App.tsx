7a43aa822218f1b3fab1cb85075303d511d86306
PellYm785 <williamliya@hotmail.fr>
2021-04-02 12:28:47 +0200

    feat: add initial presentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import gsap from 'gsap';
 import Style from './style';
 import SendIcon from './assets/send-icon.png';
 import Story from './story.json';
@@ -8,19 +9,33 @@ interface Message {
     text: string;
 }
 
-interface StoryStep {
+interface StoryStepAbstract {
     type: 'initial' | 'presentation' | 'interaction';
+    background: string;
+    next: StoryStep;
 }
 interface Step {
     background?: string;
     timer: number;
     text: string;
 }
-interface Presentation {
-    background: string;
-    steps: Step[];
-    next: StoryStep;
+
+interface Presentation extends StoryStepAbstract {
+    type: 'presentation';
 }
+interface Initial extends StoryStepAbstract {
+    type: 'initial';
+    steps: Step[];
+}
+
+interface Interaction extends StoryStepAbstract {
+    type: 'interaction';
+    messages: Message[];
+}
+
+type StoryStep = Initial | Presentation | Interaction;
+
+type StoryStepType = 'initial' | 'presentation' | 'interaction';
 
 const ReceiverMessage: React.FC<{ children: string }> = ({ children }) => (
     <div className="message receiver">
@@ -53,23 +68,63 @@ const App: React.FC = () => {
         },
     ]);
     const [responseWriting, setResponseWriting] = React.useState<boolean>(false);
-    const [stepsPresentation, setStepsPresentation] = React.useState<Step[]>([]);
+    const [stepsPresentation, setStepsPresentation] = React.useState<Step[] | null>(null);
+    const [currentStepStoryType, setCurrentStepStoryType] = React.useState<StoryStepType>(
+        'initial',
+    );
+    const [currentStep, setCurrentStep] = React.useState<Step | null>(null);
+    const filterElement = React.useRef<HTMLDivElement>(null);
 
-    const readInital = async (inital: Presentation) => {
-        setStepsPresentation(inital.steps);
+    const timeout = (ms: number) => {
+        return new Promise((resolve) => setTimeout(resolve, ms));
     };
-    const readStory = async (story: any) => {
-        switch (story.type) {
-            case 'initial':
-                await readInital(story);
-                readStory(story.next);
-                break;
-            default:
-                throw new Error('Bad element');
+
+    const changeStep = (nextStep: Step) => {
+        const t = gsap.timeline();
+        console.log(currentStep);
+        t.to(filterElement.current, { duration: 1, opacity: 0 }).call(() =>
+            setCurrentStep(nextStep),
+        );
+
+        t.to(filterElement.current, { duration: 1, opacity: 1 });
+    };
+
+    const readInital = React.useCallback(async (inital: Initial) => {
+        setCurrentStep(inital.steps[0]);
+        for (const [index, step] of Object.entries(inital.steps)) {
+            // eslint-disable-next-line no-await-in-loop
+            await timeout(step.timer);
+            changeStep(inital.steps[Number(index) + 1]);
+            console.log('update: stepsPresentation');
         }
-    };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    const readInteraction = React.useCallback(async (interaction: Interaction) => {
+        setMessageList(interaction.messages);
+    }, []);
+    const readStory = React.useCallback(
+        async (story: StoryStep) => {
+            switch (story.type) {
+                case 'initial':
+                    await readInital(story);
+                    readStory(story.next as StoryStep);
+                    break;
+                case 'interaction':
+                    await readInteraction(story);
+                    readStory(story.next as StoryStep);
+                    break;
+                default:
+                    throw new Error('Bad element');
+            }
+        },
+        [readInital, readInteraction],
+    );
 
-    readStory(Story);
+    React.useEffect(() => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        readStory(Story as Initial);
+    }, [readStory]);
 
     return (
         <Style>
@@ -105,13 +160,13 @@ const App: React.FC = () => {
                     <img src={SendIcon} alt="send" />
                 </button>
             </div>
-            <div className="presentation-screen">
-                {stepsPresentation.map((step) => (
-                    <div className="filter">
-                        <p>{step.text}</p>
+            {currentStep && (
+                <div className="presentation-screen">
+                    <div className="filter" ref={filterElement}>
+                        <p>{currentStep.text}</p>
                     </div>
-                ))}
-            </div>
+                </div>
+            )}
         </Style>
     );
 };
